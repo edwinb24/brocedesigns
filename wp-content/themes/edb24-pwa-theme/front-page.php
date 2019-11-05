4fc34b193bc00df4bd01ca4fae98c23b1a4c6787
edwinb24 <edwin_b24@hotmail.com>
2019-11-05 08:04:52 -0600

    Adding background image and positioning everything

--- wp-content/themes/edb24-pwa-theme/front-page.php
+++ wp-content/themes/edb24-pwa-theme/front-page.php
@@ -2,14 +2,16 @@
 <div class="home_page_wrapper">
 	<div class="">
 	</div>
+	<nav class="social_media">
+		
+	</nav>
 	<nav class="main_navigation"></nav>
 	<h1 class="name_and_intro">
 		<span class="name">Edwin Broce</span>
 		<span class="intro">Web Developer from Panama City, Panama</span>
 	</h1>
-	<picture class="home_picture">
-		<img src="https://ik.imagekit.io/edwinb24/main_photo_Tfbks8fvM.jpg" alt="Edwin Broce's Photo">
-	</picture>
+	<div class="home_picture">
+	</div>
 	<p class="description">
 		I am always looking for something new to learn and the opportunity to share my love for coding with others. I also love playing videogames, D&D, and teaching my cat to do tricks! 
 	</p>
