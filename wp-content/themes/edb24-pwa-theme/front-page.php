03922d44ca2aeed980e4c78c1f3d7a8418edc983
edwinb24 <edwin_b24@hotmail.com>
2019-11-06 08:27:37 -0600

    Added font and icons for social media

--- wp-content/themes/edb24-pwa-theme/front-page.php
+++ wp-content/themes/edb24-pwa-theme/front-page.php
@@ -1,11 +1,16 @@
 <?php get_header(); ?>
 <div class="home_page_wrapper">
-	<div class="">
-	</div>
 	<nav class="social_media">
-		
+		<a href="https://www.facebook.com/edwin.b24"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-facebook-f-50_9DVLQsIKi.png" alt="Facebook Account"></a>
+		<a href="https://github.com/edwinb24"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-github-50_r-WKf81AqW.png" alt="GitHub Account"></a>
+		<a href="https://www.instagram.com/edwin_b24"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-instagram-50_z5mjuv-UM.png" alt="Instagram Account"></a>
+		<a href="https://www.linkedin.com/in/edwin-broce/"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-linkedin-50_mftk4ZBV9.png" alt="LinkedIn Account"></a>
+		<a href="https://www.twitch.tv/edb24"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-twitch-50_QdGC0NQVo.png" alt="Twitch Account"></a>
+		<a href="https://twitter.com/edwin_b24"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-twitter-50_9d2UD65iv.png" alt="Twitter Account"></a>
 	</nav>
-	<nav class="main_navigation"></nav>
+	<nav class="main_navigation">
+
+	</nav>
 	<h1 class="name_and_intro">
 		<span class="name">Edwin Broce</span>
 		<span class="intro">Web Developer from Panama City, Panama</span>
