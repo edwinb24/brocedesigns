64b77f7b904c7cadafb292f889cd3b4921a872a8
edwinb24 <edwin_b24@hotmail.com>
2019-12-31 07:16:25 -0600

    3D Scrolling Achieved! Moving to tweaking things

--- wp-content/themes/edb24-pwa-theme/assets/preprocess/scripts/3d_scrolling.js
+++ wp-content/themes/edb24-pwa-theme/assets/preprocess/scripts/3d_scrolling.js
@@ -1,1 +1,30 @@
-console.log("testt")
+// setSceneHeight emulate the Z- axis depth, since we are scrolling vertically
+// the height combine how fast our scrolling is our camera perspective, etc. and 
+// make everything come together.
+
+document.addEventListener("DOMContentLoaded", () => { setSceneHeight()})
+
+function setSceneHeight () {
+    console.log("Im working1")
+
+    let list_of_items = document.getElementsByClassName("jobs")
+    let number_of_items = list_of_items.length
+    let item_z_index = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--itemZ"))
+    let perspective = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--scenePerspective"))
+    let camera_speed = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--cameraSpeed"))
+    let height = window.innerHeight + perspective * camera_speed * item_z_index * camera_speed * number_of_items
+    document.documentElement.style.setProperty("--viewportHeight", height)
+}
+
+// moveCamera simulate the movement of the camera by converting the user 
+//scrolling onto movement on a 3D space
+
+document.addEventListener("DOMContentLoaded", () => {
+    window.addEventListener("scroll", moveCamera)
+    setSceneHeight()
+})
+
+function moveCamera() {
+    document.documentElement.style.setProperty("--cameraZ", window.pageYOffset)
+    console.log("Im working2")
+}
