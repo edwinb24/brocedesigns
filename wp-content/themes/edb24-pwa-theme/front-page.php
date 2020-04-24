2e127923e67c7bc190e1b69f0a163c105e737845
edwinb24 <edwin_b24@hotmail.com>
2020-04-24 11:08:06 -0500

    Corrected link to Youtube Channel

--- wp-content/themes/edb24-pwa-theme/front-page.php
+++ wp-content/themes/edb24-pwa-theme/front-page.php
@@ -6,7 +6,7 @@
 		<a href="https://github.com/edwinb24" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-github-50_2tvaKuTdI.png" alt="Go to My GitHub"></a>
 		<a href="https://www.instagram.com/edwin_b24" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-instagram-50_G9KosLytl.png" alt="Go to My Instagram"></a>
 		<a href="https://www.linkedin.com/in/edwin-broce/" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-linkedin-50_TMqTNg4N8w.png" alt="Go to My LinkedIn"></a>
-		<a href="https://www.youtube.com/channel/UC751iWBodxoX3rYUleWRaHA" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-play-button-50_e4sHdo05n.png" alt="Go to My YouTube"></a>
+		<a href="https://www.youtube.com/channel/UC316MzN9QyFvGJisgyjQWzw/" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-play-button-50_e4sHdo05n.png" alt="Go to My YouTube"></a>
 		<a href="https://twitter.com/edwin_b24" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-twitter-50_4MdNhJlT4.png" alt="Go to My Twitter"></a>
 	</nav>
 	<nav class="main_navigation"  aria-label="Main Navigation">
