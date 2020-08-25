b208526e26afb0ff117fa54aca8eb9ef2f2bc15b
edwinb24 <edwin_b24@hotmail.com>
2020-08-24 19:08:51 -0500

    Correcting images links after changing website's name

--- wp-content/themes/edb24-pwa-theme/front-page.php
+++ wp-content/themes/edb24-pwa-theme/front-page.php
@@ -2,14 +2,14 @@
 <title>Edwin Broce's Website</title>
 <div class="home_page_wrapper">
 	<nav class="social_media" aria-label="Social Media Navigation">
-		<a href="https://www.facebook.com/edwin.b24" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-facebook-f-50_f5jaI8SnuZ.png" alt="Go to My Facebook"></a>
-		<a href="https://github.com/edwinb24" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-github-50_2tvaKuTdI.png" alt="Go to My GitHub"></a>
-		<a href="https://www.instagram.com/edwin_b24" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-instagram-50_G9KosLytl.png" alt="Go to My Instagram"></a>
-		<a href="https://www.linkedin.com/in/edwin-broce/" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-linkedin-50_TMqTNg4N8w.png" alt="Go to My LinkedIn"></a>
-		<a href="https://www.youtube.com/channel/UC316MzN9QyFvGJisgyjQWzw/" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-play-button-50_e4sHdo05n.png" alt="Go to My YouTube"></a>
-		<a href="https://twitter.com/edwin_b24" target="_blank"  rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-twitter-50_4MdNhJlT4.png" alt="Go to My Twitter"></a>
+		<a href="https://www.facebook.com/edwin.b24" target="_blank" rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-facebook-f-50_f5jaI8SnuZ.png" alt="Go to My Facebook"></a>
+		<a href="https://github.com/edwinb24" target="_blank" rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-github-50_2tvaKuTdI.png" alt="Go to My GitHub"></a>
+		<a href="https://www.instagram.com/edwin_b24" target="_blank" rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-instagram-50_G9KosLytl.png" alt="Go to My Instagram"></a>
+		<a href="https://www.linkedin.com/in/edwin-broce/" target="_blank" rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-linkedin-50_TMqTNg4N8w.png" alt="Go to My LinkedIn"></a>
+		<a href="https://www.youtube.com/channel/UC316MzN9QyFvGJisgyjQWzw/" target="_blank" rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-play-button-50_e4sHdo05n.png" alt="Go to My YouTube"></a>
+		<a href="https://twitter.com/edwin_b24" target="_blank" rel="nofollow noreferrer"><img src="https://ik.imagekit.io/edwinb24/Social_Media_Icons/icons8-twitter-50_4MdNhJlT4.png" alt="Go to My Twitter"></a>
 	</nav>
-	<nav class="main_navigation"  aria-label="Main Navigation">
+	<nav class="main_navigation" aria-label="Main Navigation">
 		<div role="button"><a href="/jobs">Experience</a></div>
 		<div role="button"><a href="/projects">Portfolio</a></div>
 		<div role="button"><a href="https://www.youtube.com/channel/UC316MzN9QyFvGJisgyjQWzw/">Tutorials</a></div>
@@ -18,11 +18,10 @@
 		<span class="name">Edwin Broce</span>
 		<span class="intro">Web Developer from Panama City, Panama</span>
 	</h1>
-	<div class="home_picture lazy_loaded_image" style='background-image: url("<?php echo $GLOBALS[ 'img_path']; ?>2020/01/home_page_photo.jpg?tr=w-20,h-20 ")'>
+	<div class="home_picture lazy_loaded_image" style='background-image: url("<?php echo $GLOBALS['img_path']; ?>2020/08/home_page_photo.jpg?tr=w-20,h-20 ")'>
 	</div>
 	<p class="description">
-		I am always looking for something new to learn and the opportunity to share my love for coding with others. I also love playing videogames, D&D, and teaching my cat to do tricks! 
+		I am always looking for something new to learn and the opportunity to share my love for coding with others. I also love playing videogames, D&D, and teaching my cat to do tricks!
 	</p>
 </div>
 <?php get_footer(); ?>
-
